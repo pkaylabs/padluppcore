9bd4e32de995cf8df7abaf16d8d521dc551bd8e5
princek <princesamuelpks@gmail>
2025-12-12 08:26:46 +0000

    update

--- padluppcore/settings.py
+++ padluppcore/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-#v!3(v3lc@y(yu)u@8vhp5gf2jg2hgn9-qpff#-9gar#$u)+oh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
